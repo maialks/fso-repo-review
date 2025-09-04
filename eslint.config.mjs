70e22edcc0db178bd01cb0656d70e2be4a16c819
maialks <lucasmss01ita@gmail.com>
2025-09-04 07:35:00 -0300

    added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import reactNative from "eslint-plugin-react-native";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react,
+      "react-native": reactNative,
+      prettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react-native/no-unused-styles": "warn",
+      "react-native/no-inline-styles": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
